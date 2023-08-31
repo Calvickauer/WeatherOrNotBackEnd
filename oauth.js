652ed6cefa9a541337ddc2513238479cefe53b82
calvickauer <calvickauer@gmail.com>
2023-08-30 23:57:04 -0700

    update OAuth error handling / security

--- oauth.js
+++ oauth.js
@@ -3,11 +3,13 @@ const OAuth2Strategy = require('passport-oauth2').Strategy;
 const session = require('express-session');
 
 // Configure session middleware
-app.use(session({
-  secret: 'your-secret',
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: 'your-secret',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // Initialize Passport middleware
 app.use(passport.initialize());
@@ -20,15 +22,19 @@ passport.use(
     {
       authorizationURL: 'https://provider.com/oauth2/authorize',
       tokenURL: 'https://provider.com/oauth2/token',
-      clientID: 'your-client-id',
-      clientSecret: 'your-client-secret',
+      clientID: process.env.CLIENT_ID, // Use environment variables
+      clientSecret: process.env.CLIENT_SECRET, // Use environment variables
       callbackURL: 'http://your-app/callback', // Replace with your callback URL
     },
     (accessToken, refreshToken, profile, done) => {
       // Handle the authentication callback
-      // You can store the user details in your database or session
-      // Call the `done` function to indicate successful authentication
-      done(null, profile);
+      try {
+        // Save user data to your database or session
+        // For example, save to session: req.session.user = profile;
+        done(null, profile);
+      } catch (error) {
+        done(error);
+      }
     }
   )
 );
@@ -42,4 +48,24 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
   // Deserialize user details
   done(null, user);
+});
+
+// Check Authentication Middleware
+const isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next(); // Proceed to the next middleware
+  }
+  res.redirect('/login'); // Redirect to login page if not authenticated
+};
+
+// Logout Route
+app.get('/logout', (req, res) => {
+  req.logout(); // Passport function to clear session
+  res.redirect('/'); // Redirect to home page after logout
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('An error occurred.');
 });
